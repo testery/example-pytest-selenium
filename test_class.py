a4134cf216167a26ec36375f61c3056437dcf3ae
Aaron Freeman <aaron@freeman.zone>
2021-02-13 10:52:09 -0600

    adding parameterized test that causes issue

--- test_class.py
+++ test_class.py
@@ -1,6 +1,18 @@
 import pytest
 
 
+@pytest.mark.parametrize(
+    ("value_one", "value_two"),
+    [
+        ("a", "a"),
+        ("b", "b"),
+        (True, True),
+    ],
+)
+def test_truth(value_one, value_two):
+    assert value_one == value_two
+
+
 class TestClass:
     def test_one(self):
         print("Hello from test_one")
