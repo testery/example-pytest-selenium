0fda15e3d427092deeeb8699ecdb0a4091b650ef
SeanRayR <srelacion@fullscale.io>
2024-05-27 22:49:32 +0800

    update conftest chrome

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture
 def chrome_browser():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")  # Run Chrome in headless mode
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    service = webdriver.ChromeService()
+    driver = webdriver.Chrome(options=options, service=service)
     yield driver
     driver.quit()
