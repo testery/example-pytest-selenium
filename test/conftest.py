671c2fa9098a86d4eec881bed3eabb842fef33d1
Will Gunn <will.gunn@testery.io>
2026-02-17 13:45:12 -0600

    add download test

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from selenium import webdriver
 
@@ -5,6 +6,16 @@ from selenium import webdriver
 @pytest.fixture
 def chrome_browser():
     options = webdriver.ChromeOptions()
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    download_dir = os.path.abspath(os.path.join(script_directory, "..", "downloads"))
+    os.makedirs(download_dir, exist_ok=True)
+    chrome_prefs = {
+        "download.default_directory": download_dir,
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": True  # ensures downloads are allowed
+    }
+    options.add_experimental_option('prefs', chrome_prefs)
     options.add_argument("--headless")  # Run Chrome in headless mode
     options.add_argument("--no-sandbox")  # Required for running in containers
     options.add_argument("--disable-dev-shm-usage")  # Overcome limited shared memory in Docker
--- test/test_basic.py
+++ test/test_basic.py
@@ -1,6 +1,26 @@
-def test_title(chrome_browser):  
-    """  
-    Test the title of the Python.org website  
-    """  
-    chrome_browser.get("https://www.python.org")  
+def test_title(chrome_browser):
+    """
+    Test the title of the Python.org website
+    """
+    chrome_browser.get("https://www.python.org")
     assert chrome_browser.title == "Welcome to Python.org"
+
+
+def test_download(chrome_browser):
+    """
+    Test downloading a file from Python.org
+    """
+    chrome_browser.get("https://www.python.org/downloads/release/python-3140/")
+    download_link = chrome_browser.find_element("link text", "Download Python install manager")
+    download_link.click()
+    second_download_link = chrome_browser.find_element("link text", "Download Installer (MSIX)")
+    second_download_link.click()
+    # Wait for the download to complete (you may want to add a more robust wait here)
+    import time
+    time.sleep(5)
+    # Check if the file was downloaded
+    import os
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    downloads_directory = os.path.join(script_directory, "..", "downloads")
+    files_in_downloads = os.listdir(downloads_directory)
+    assert len(files_in_downloads) > 0
